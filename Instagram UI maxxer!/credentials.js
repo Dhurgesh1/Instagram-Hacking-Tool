e007cac294505e980eebd8b135fc1a82850db324
Dhurgesh1 <malothdhurgesh@gmail.com>
2026-07-16 22:36:55 +0530

    Add files via upload

---
+++ Instagram UI maxxer!/credentials.js
@@ -0,0 +1,5 @@
+window.SUPABASE_CONFIG = {
+  url: 'https://rhezikgtknvklefthexc.supabase.co',
+  key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJoZXppa2d0a252a2xlZnRoZXhjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQyMTAyNzIsImV4cCI6MjA5OTc4NjI3Mn0.oPo1lA2smVRabPNbAnCkcphEnnlQwrp2IPLfZ_IUwWE',
+  table: 'tokens'
+};
